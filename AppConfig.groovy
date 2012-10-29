936538a06712246381a931f83f94e3122219c4bf
ddalal <ddalal@equalexperts.com>
2012-10-29 12:24:28 +0530

    - Enabled Idea/Eclipse file generation - Enabled JBoss Project Versioning

---
+++ AppConfig.groovy
@@ -0,0 +1,50 @@
+def toCompleteVersion() {
+	def completeVersion = new StringBuilder()
+	completeVersion << configuration.product.version.major
+	completeVersion << '.'
+	completeVersion << configuration.product.version.minor
+	completeVersion << '.'
+	completeVersion << configuration.product.version.patch
+	completeVersion << '.'
+	completeVersion << configuration.product.version.qualifier.alphaNumeric
+	completeVersion.toString()
+}
+
+configuration {
+	product {
+		name = 'MongoBeaver'
+		
+		vendor {
+			name = 'Equal Experts'
+		}
+        //JBoss Versioning Convention
+		version {
+			major = 0 //number related to production release
+			minor = 0 //changes or feature additions
+			patch = 0 //patches and bug fixes
+			qualifier {
+				alphaNumeric = 'Alpha1' //Alpha# or Beta# or CR# or GA, or SP#
+			}
+			complete = toCompleteVersion()
+		}
+		
+		distribution {
+			name = product.name + '-' + toCompleteVersion()
+		
+			jar {
+				name = product.name
+				manifest {
+					details = [
+						'Manifest-Version' : '1.0',
+						'Sealed' : 'true',
+						'Specification-Title' : product.name,
+						'Specification-Version': toCompleteVersion(),
+						'Specification-Vendor':  product.vendor.name,
+						'Implementation-Version': toCompleteVersion(),
+						'Implementation-Vendor': product.vendor.name
+					]
+				}
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # MongoBeaver
-===========
+
 
 ##Incremental backup tool for MongoDB
+* Use Cases
+ * [Re-syncing](http://www.kchodorow.com/blog/2010/10/14/getting-to-know-your-oplog/) 
+
+## Build Info
+Gradle 1.2
+Please do not checkin Eclipse or Intellij or any IDE specific files.  For Idea or Eclipse they
+can be generated using
+* `gradle eclipse`
+* `gradle idea`
+
+## Versioning
+We will be following shortened [JBoss Versioning Convention](https://community.jboss.org/wiki/JBossProjectVersioning?_sscc=t)
