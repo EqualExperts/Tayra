cd2c0d3c650366dcd7d932206978c33daf3bd8f3
Swanand Pethe <spethe@equalexperts.com>
2012-11-01 18:36:14 +0530

    Read Inserted Documents

---
+++ src/test/java/com/ee/beaver/InsertDocument.java
@@ -0,0 +1,12 @@
+package com.ee.beaver;
+
+public class InsertDocument {
+
+	public String _id;
+	public String name;
+
+	public InsertDocument(String _id, String name) {
+		this._id = _id;
+		this.name = name;
+	}
+}
--- src/test/java/com/ee/beaver/ReadOperationsSpecs.java
+++ src/test/java/com/ee/beaver/ReadOperationsSpecs.java
@@ -5,7 +5,9 @@ import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import java.util.Date;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.bson.types.BSONTimestamp;
 import org.junit.Before;
@@ -63,5 +65,68 @@ public class ReadOperationsSpecs {
 
 		//When-Then
 		assertThat(reader.readDocument(), nullValue());
+	}
+	
+	@Test
+	public void itReadsOneInsertCollectionOperation() {
+		// Given
+		given(mockOplogCollectionIterator.hasNext()).willReturn(true);
+
+		OplogDocument entry = new OplogDocument();
+		entry.ts = new BSONTimestamp(123456, 0);
+		entry.h = 5881556024799929122L;
+		entry.op = "i";
+		entry.ns = "ee.people";
+		entry.o = new InsertDocument("ObjectId(\"50920e139826a65153560b30\")",
+				"swanand");
+
+		given(mockOplogCollectionIterator.next()).willReturn(entry);
+
+		// When
+		OplogDocument oplogDocument = reader.readDocument();
+
+		// Then
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		String expected = gson.toJson(entry).replaceAll("\\\\", "");
+		
+		assertThat(oplogDocument.toJson(), is(expected));
+	}
+
+	@Test
+	public void itReadsMultipleInsertCollectionOperation() {
+		// Given
+		given(mockOplogCollectionIterator.hasNext()).willReturn(true);
+
+		OplogDocument entry1 = new OplogDocument();
+		entry1.ts = new BSONTimestamp(123456, 0);
+		entry1.h = 5881556024799929122L;
+		entry1.op = "i";
+		entry1.ns = "ee.people";
+		entry1.o = new InsertDocument("ObjectId(\"50920e139826a65153560b30\")",
+				"test1");
+
+		OplogDocument entry2 = new OplogDocument();
+		entry2.ts = new BSONTimestamp(123456, 0);
+		entry2.h = 5881556024799929122L;
+		entry2.op = "i";
+		entry2.ns = "ee.people";
+		entry2.o = new InsertDocument("ObjectId(\"50920e139826a65153560b30\")",
+				"testUser 2");
+
+		List<OplogDocument> insertDocuments = new ArrayList<OplogDocument>();
+		insertDocuments.add(entry1);
+		insertDocuments.add(entry2);
+
+		given(mockOplogCollectionIterator.next()).willReturn(entry1)
+				.willReturn(entry2);
+
+		// When-Then
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		for (OplogDocument insertDocument : insertDocuments) {
+			OplogDocument oplogDocument = reader.readDocument();
+			String expected = gson.toJson(insertDocument).replaceAll("\\\\", "");
+
+			assertThat(oplogDocument.toJson(), is(expected));
+		}
 	}
 }
