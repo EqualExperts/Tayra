92569e979dbfe5daa4360927bbf4485ad4813bac
DhavalDalal <ddalal@equalexperts.com>
2012-11-07 17:05:28 +0530

    Introduced TimestampWriter

--- src/main/java/com/ee/beaver/io/TimestampWriter.java
+++ src/main/java/com/ee/beaver/io/TimestampWriter.java
@@ -4,34 +4,35 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class TimestampWriter extends Writer {
-	
-	private final Writer destination;
-	private final Writer delegateTarget;
+    private final Writer destination;
+    private final Writer delegateTarget;
 
-	public TimestampWriter(final Writer destination, final Writer delegateTarget) {
-		this.destination = destination;
-		this.delegateTarget = delegateTarget;
-	}
+    public TimestampWriter(final Writer destination,
+      final Writer delegateTarget) {
+        this.destination = destination;
+        this.delegateTarget = delegateTarget;
+    }
 
-	@Override
-	public void write(char[] data, int off, int len) throws IOException {
-		delegateTarget.write(data, off, len);
-		char[] ts = extractTimestamp(data);
-		destination.write(ts, off, len);
-	}
+    @Override
+    public final void write(final char[] data, final int off, final int len)
+      throws IOException {
+        delegateTarget.write(data, off, len);
+        char[] ts = extractTimestamp(data);
+        destination.write(ts, off, ts.length);
+    }
 
-	private char[] extractTimestamp(char[] data) {
-		String[] splitData = new String(data).split(",");
-		String ts = splitData[0] + " , " + splitData[1];
-		System.out.println(ts.substring(2));
-		return ts.substring(2).toCharArray();
-	}
+    private char[] extractTimestamp(final char[] data) {
+        String oplogDocument = new String(data);
+        String ts = oplogDocument.substring(oplogDocument.indexOf("\"ts\" :"),
+            oplogDocument.indexOf("}") + 1);
+        return ts.toCharArray();
+    }
 
-	@Override
-	public void flush() throws IOException {
-	}
+    @Override
+    public void flush() throws IOException {
+    }
 
-	@Override
-	public void close() throws IOException {
-	}
+    @Override
+    public void close() throws IOException {
+    }
 }
--- src/test/java/com/ee/beaver/io/TimestampWriterSpecs.java
+++ src/test/java/com/ee/beaver/io/TimestampWriterSpecs.java
@@ -2,6 +2,9 @@ package com.ee.beaver.io;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -11,8 +14,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import static org.mockito.BDDMockito.*;
-import static org.mockito.Mockito.*;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -20,25 +21,32 @@ public class TimestampWriterSpecs {
 
 	@Mock
 	private Writer mockTargetWriter;
-	private Writer destination;
+	private StringWriter destination;
 	private TimestampWriter timestampWriter;
 	private char[] data;
 
 	@Before
 	public void setup() {
-		data = new String("{ \"ts\" : { \"$ts\" : 1352094941 , \"$inc\" : 1} , \"h\" : -5637503227244014604 , \"op\" : \"i\" , \"ns\" : \"person.things\" , \"o\" : { \"_id\" : { \"$oid\" : \"509754dd2862862d511f6b57\"} , \"name\" : \"xyz\"}}").toCharArray();
+		data = new StringBuilder()
+				.append("{ \"ts\" : { \"$ts\" : 1352094941 , \"$inc\" : 1} , ")
+				.append("\"h\" : -5637503227244014604 , \"op\" : \"i\" ,")
+				.append(" \"ns\" : \"person.things\" , \"o\" : { \"_id\"")
+				.append(" : { \"$oid\" : \"509754dd2862862d511f6b57\"} ,")
+				.append(" \"name\" : \"xyz\"}}")
+				.toString()
+				.toCharArray();
 		destination = new StringWriter();
 		timestampWriter = new TimestampWriter(destination, mockTargetWriter);
 	}
 
 	@Test
 	public void writesTimestamptoDestination() throws IOException {
-		//When
+		// When
 		timestampWriter.write(data, 0, data.length);
 
-		//Then
-		String expectedTimestamp = ("\"ts\" : { \"$ts\" : 1352094941 , \"$inc\" : 1}\"");
-		System.out.println("dest" + destination.toString());
+		// Then
+		String expectedTimestamp = ("\"ts\" : { \"$ts\" : 1352094941 ," +
+				" \"$inc\" : 1}");
 		assertThat(destination.toString(), is(expectedTimestamp));
 	}
 
@@ -61,10 +69,10 @@ public class TimestampWriterSpecs {
 		// When
 		try {
 			timestampWriter.write(data, 0, data.length);
+			fail("Should not have written timestamp!");
 		} catch (IOException e) {
 			// Then
 			assertThat(destination.toString(), is(""));
 		}
 	}
-
 }
