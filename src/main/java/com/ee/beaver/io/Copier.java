13cd40c082fcbd5fc0124442b9ce208363eb3dfb
DhavalDalal <ddalal@equalexperts.com>
2012-11-15 19:10:04 +0530

    Refactored to Notifier

--- src/main/java/com/ee/beaver/io/Copier.java
+++ src/main/java/com/ee/beaver/io/Copier.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Writer;
 
-import fj.Effect;
-import static fj.data.List.list;
 
 public class Copier {
 
@@ -24,59 +22,20 @@ public class Copier {
 
   public final void copy(final BufferedReader from, final OplogReplayer to,
     final CopyListener... listeners) {
+    Notifier notifier = new Notifier(listeners);
     String document = null;
     try {
       while ((document = from.readLine()) != null) {
-        notifyReadSuccess(document, listeners);
+        notifier.notifyReadSuccess(document);
         try {
           to.replayDocument(document);
-          notifyWriteSuccess(document, listeners);
+          notifier.notifyWriteSuccess(document);
         } catch (RuntimeException problem) {
-          notifyWriteFailure(document, problem, listeners);
+          notifier.notifyWriteFailure(document, problem);
         }
       }
     } catch (IOException ioe) {
-      notifyReadFailure(null, ioe, listeners);
+      notifier.notifyReadFailure(null, ioe);
     }
-  }
-
-  private void notifyReadFailure(final String document, final IOException prob,
-    final CopyListener... listeners) {
-    list(listeners).foreach(new Effect<CopyListener>() {
-      @Override
-      public void e(final CopyListener listener) {
-        listener.onReadFailure(document, prob);
-      }
-    });
-  }
-
-  private void notifyWriteFailure(final String document, final Throwable prob,
-    final CopyListener... listeners) {
-    list(listeners).foreach(new Effect<CopyListener>() {
-      @Override
-      public void e(final CopyListener listener) {
-        listener.onWriteFailure(document, prob);
-      }
-    });
-  }
-
-  private void notifyWriteSuccess(final String document,
-    final CopyListener... listeners) {
-    list(listeners).foreach(new Effect<CopyListener>() {
-      @Override
-      public void e(final CopyListener listener) {
-        listener.onWriteSuccess(document);
-      }
-    });
-  }
-
-  private void notifyReadSuccess(final String document,
-    final CopyListener... listeners) {
-    list(listeners).foreach(new Effect<CopyListener>() {
-      @Override
-      public void e(final CopyListener listener) {
-        listener.onReadSuccess(document);
-      }
-    });
   }
 }
---
+++ src/main/java/com/ee/beaver/io/Notifier.java
@@ -0,0 +1,55 @@
+package com.ee.beaver.io;
+
+import static fj.data.List.list;
+
+import java.io.IOException;
+
+import fj.Effect;
+
+public class Notifier {
+
+  private final CopyListener[] listeners;
+
+  public Notifier(final CopyListener... listeners) {
+    this.listeners = listeners;
+  }
+
+  public final void notifyReadSuccess(final String document) {
+    list(listeners).foreach(new Effect<CopyListener>() {
+      @Override
+      public void e(final CopyListener listener) {
+        listener.onReadSuccess(document);
+      }
+    });
+  }
+
+  public final void notifyWriteSuccess(final String document) {
+    list(listeners).foreach(new Effect<CopyListener>() {
+      @Override
+      public void e(final CopyListener listener) {
+        listener.onWriteSuccess(document);
+      }
+    });
+  }
+
+  public final void notifyWriteFailure(final String document,
+    final Throwable problem) {
+    list(listeners).foreach(new Effect<CopyListener>() {
+      @Override
+      public void e(final CopyListener listener) {
+        listener.onWriteFailure(document, problem);
+      }
+    });
+  }
+
+  public final void notifyReadFailure(final String document,
+  final IOException problem) {
+    list(listeners).foreach(new Effect<CopyListener>() {
+      @Override
+      public void e(final CopyListener listener) {
+        listener.onReadFailure(document, problem);
+      }
+    });
+  }
+
+}
