3710883adcff2dec749af49c139de52574113bf1
dhaval.dalal <dhaval.dalal@software-artisan.com>
2012-11-22 14:31:37 +0530

    Removed Dos Gremlins from Shell Scripts

--- distribution-template/backup.sh
+++ distribution-template/backup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp %%JAVA_CLASS_PATH%% com.ee.beaver.runner.Runner "backup" $@
+java -cp libs/commons-cli-1.1.jar:libs/functionaljava-3.1.jar:libs/groovy-all-2.0.4.jar:libs/mongo-java-driver-2.7.3.jar:libs/MongoBeaver-0.3.0.Alpha1.jar com.ee.beaver.runner.Runner "backup" $@
