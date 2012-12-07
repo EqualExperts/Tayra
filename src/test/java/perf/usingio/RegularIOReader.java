97f6cd9b131fdb0e47ddde73a58445e39944359d
Sameer Soni <ssoni@equalexperts.com>
2012-12-07 15:41:35 +0530

    Started Performance Testing

--- src/test/java/perf/usingio/RegularIOReader.java
+++ src/test/java/perf/usingio/RegularIOReader.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.util.Date;
 
 public final class RegularIOReader {
-//  private static int count = 1010241024; // 10 MB
 
   private static final int TIMES = 8;
   private static final int ONE_KB = 1024;
@@ -14,7 +13,6 @@ public final class RegularIOReader {
   private RegularIOReader() {
   }
 
-  @SuppressWarnings("unused")
   public static void main(final String[] args) throws Exception {
 
     int bufferSize = (TIMES * ONE_KB * ONE_KB);
@@ -26,7 +24,6 @@ public final class RegularIOReader {
     String strLine = br.readLine();
     while (strLine != null) {
       strLine = br.readLine();
-      // System.out.println (strLine);
     }
     long endTime = new Date().getTime();
     System.out.printf("%s, %d\n", file.getName(), (endTime - startTime));
--- src/test/java/perf/usingnio/MemoryMappedReader.java
+++ src/test/java/perf/usingnio/MemoryMappedReader.java
@@ -1,36 +1,43 @@
 package usingnio;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Date;
 
 public final class MemoryMappedReader {
-//  private static int count = 1010241024; // 10 MB
+
+  private static final int BUFFER_SIZE = 67108864;
 
   private MemoryMappedReader() {
-
   }
 
-  @SuppressWarnings("unused")
-  public static void main(final String[] args) throws Exception {
+  public static void main(final String[] args) throws IOException {
     String name = args[0];
     File file = new File(name);
-    RandomAccessFile memoryMappedFile = new RandomAccessFile(file, "r");
 
+    // use RandomAccessFile because it supports readFully()
+    RandomAccessFile fileHandle = new RandomAccessFile(file, "r");
+    fileHandle.seek(0L);
+    int readSize = 0;
     long startTime = new Date().getTime();
-    // Mapping a file into memory
-    MappedByteBuffer out = memoryMappedFile.getChannel().map(
-        FileChannel.MapMode.READ_ONLY, 0, file.length());
+    while (fileHandle.getFilePointer() < fileHandle.length()) {
+      readSize = (int) Math.min(BUFFER_SIZE,
+            fileHandle.length() - fileHandle.getFilePointer());
+      MappedByteBuffer buffer = fileHandle.getChannel().map(
+            FileChannel.MapMode.READ_ONLY,
+            fileHandle.getFilePointer(), readSize);
 
-    // Reading from Memory Mapped File
-    while (out.hasRemaining()) {
-      byte data = out.get();
-      // System.out.print((char)data);
+      // Reading from Memory Mapped File
+      while (buffer.hasRemaining()) {
+        buffer.get();
+      }
+      fileHandle.seek(fileHandle.getFilePointer() + readSize);
     }
     long endTime = new Date().getTime();
+    fileHandle.close();
     System.out.printf("%s, %d\n", file.getName(), (endTime - startTime));
   }
-
 }
