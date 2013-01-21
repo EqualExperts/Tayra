922e26e6a8204337c40dc40bbd7aff477a454619
DhavalDalal <ddalal@equalexperts.com>
2013-01-21 18:43:04 +0530

    Cleaned up duplicate code

--- src/main/groovy/com/ee/beaver/runner/Runner.groovy
+++ src/main/groovy/com/ee/beaver/runner/Runner.groovy
@@ -3,13 +3,11 @@ package com.ee.beaver.runner
 import com.ee.beaver.command.*
 
 def scriptName = args[0]
+Binding context = new Binding()
+context.setVariable('args', args[1..<args.length])
 if (scriptName == 'backup') {
-  Binding context = new Binding()
-  context.setVariable('args', args[1..<args.length])
   script = new Backup(context)
 } else if (scriptName == 'restore') { 
-	Binding context = new Binding()
-	context.setVariable('args', args[1..<args.length])
 	script = new Restore(context)
 } else {
   throw new IllegalArgumentException("Don't know how to process: $scriptName")
