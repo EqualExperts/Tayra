cfa0ab0c60a1492e27e3beeb00d136e70d47c77b
Bhagyashree <bhagyashree.mandora@gmail.com>
2013-01-23 12:34:19 +0530

    resolved drop index version incompatibility

--- src/main/java/com/ee/tayra/domain/operation/DropIndex.java
+++ src/main/java/com/ee/tayra/domain/operation/DropIndex.java
@@ -15,22 +15,53 @@ public class DropIndex implements SchemaOperation {
     String collectionName = (String) spec.get("deleteIndexes");
     List<DBObject> indexes = db.getCollection(collectionName)
         .getIndexInfo();
+
     if (spec.get("index").equals("*")) {
       db.getCollection(collectionName).dropIndexes();
     } else {
-      DBObject indexObj = (DBObject) spec.get("index");
-      Set<String> indexKeys = indexObj.keySet();
+      if (versionLowerThan2_2_1(spec)) {
+        String indexName = (String) spec.get("index");
+        if (doesNotExist(indexes, indexName)) {
+          throw new DropIndexFailed("Cannot drop index : "
+            + indexName + " Index doesn't exist.");
+        }
+        db.getCollection(collectionName).dropIndex(indexName);
+      } else {
+           DBObject indexObj = (DBObject) spec.get("index");
+           toMongoFormat(indexObj);
+           if (doesNotExist(indexes, indexObj)) {
+             throw new DropIndexFailed("Cannot drop index : "
+               + indexObj.toString() + " Index doesn't exist.");
+           }
+          db.getCollection(collectionName).dropIndex(indexObj);
+        }
+    }
+  }
+
+  private boolean versionLowerThan2_2_1(final DBObject spec) {
+    return spec.get("index") instanceof String;
+  }
+
+  private void toMongoFormat(final DBObject indexObj) {
+    Set<String> indexKeys = indexObj.keySet();
       for (String indexKey : indexKeys) {
         Double indexValue = (Double) indexObj.get(indexKey);
         indexObj.put(indexKey, indexValue.intValue());
       }
-      if (doesNotExist(indexes, indexObj)) {
-        throw new DropIndexFailed("Cannot drop index : "
-            + indexObj.toString() + " Index doesn't exist.");
-      }
-      db.getCollection(collectionName).dropIndex(indexObj);
-    }
   }
+
+  private boolean doesNotExist(final List<DBObject> existingIndexList,
+      final String indexName) {
+    DBObject[] indexes = new DBObject[existingIndexList.size()];
+    existingIndexList.toArray(indexes);
+    return !list(indexes).exists(new F<DBObject, Boolean>() {
+      @Override
+      public Boolean f(final DBObject document) {
+        String keyName = (String) document.get("name");
+        return keyName.equals(indexName);
+      }
+    });
+}
 
   private boolean doesNotExist(final List<DBObject> existingIndexList,
       final DBObject indexObj) {
--- src/test/groovy/com/ee/tayra/domain/operation/DropIndexSpecs.groovy
+++ src/test/groovy/com/ee/tayra/domain/operation/DropIndexSpecs.groovy
@@ -129,4 +129,42 @@ class DropIndexSpecs extends RequiresMongoConnection{
    standalone.getDB(dbName).getCollection(collectionName).getIndexInfo().size() == 1
   
  }
+
+ def dropsASimpleIndexBeforeVersion2_2_1() {
+	 def key = new BasicDBObjectBuilder()
+		.start()
+		 .add('roll', 1)
+		.get()
+	 standalone.getDB(dbName).getCollection(collectionName).ensureIndex(key)
+	 
+	 given: 'drop index oplog entry'
+	  DBObject spec = new BasicDBObjectBuilder()
+		 .start()
+		  .add('deleteIndexes', collectionName)
+	      .add('index', 'roll_1')
+		 .get()
+		 
+	 when: 'Operation runs'
+	  operation.doExecute(standalone.getDB(dbName), spec)
+	 
+	 then: 'Index should not be present'
+	   assertThatIndexIsNotPresent(spec)
+	}
+ 
+ def shoutsWhenASimpleIndexBeforeVersion2_2_1_DoesNotExist() {
+	 given: 'drop index oplog entry'
+		  DBObject spec = new BasicDBObjectBuilder()
+			 .start()
+			  .add('deleteIndexes', collectionName)
+			  .add('index', 'roll_1')
+		 .get()
+		 
+	 when: 'Operation runs'
+	  		operation.doExecute(standalone.getDB(dbName), spec)
+	 
+	 then: 'it complains that index to be dropped does not exist'
+	 	def problem = thrown(DropIndexFailed)
+		 problem.message == "Cannot drop index : " + spec.get('index').toString() + " Index doesn't exist."
+	}
+
 }
