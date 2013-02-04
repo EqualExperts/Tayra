953797ec5d4a04bf97df247d9b0262a48352a272
Swanand Pethe <spethe@equalexperts.com>
2013-02-04 18:54:21 +0530

    Refactored Dbcriteria and Timestamp criteria specs

--- src/test/groovy/com/ee/tayra/io/criteria/DbCriteriaSpecs.groovy
+++ src/test/groovy/com/ee/tayra/io/criteria/DbCriteriaSpecs.groovy
@@ -5,25 +5,21 @@ import com.ee.tayra.io.criteria.DbCriteria;
 import spock.lang.*
 class DbCriteriaSpecs extends Specification {
 
-	def documentOne = '{ "ts" : { "$ts" : 1357537752 , "$inc" : 1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-	def documentTwo = '{ "ts" : { "$ts" : 1357537752 , "$inc" : 1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "person.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-	def documentThree = '{ "ts" : { "$ts" : 1357213610 , "$inc" : 1} , "h" : 0 , "v" : 2 , "op" : "n" , "ns" : "" , "o" : { "msg" : "initiating set"}}'
+	static documentOne = '{ "ts" : { "$ts" : 1357537752 , "$inc" : 1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
+	static documentTwo = '{ "ts" : { "$ts" : 1357537752 , "$inc" : 1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "person.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
+	static configurationEntry = '{ "ts" : { "$ts" : 1357213610 , "$inc" : 1} , "h" : 0 , "v" : 2 , "op" : "n" , "ns" : "" , "o" : { "msg" : "initiating set"}}'
 	def criteria
 	def dbName = 'test'
 
-	def returnsTrueIfCriteriaForASpecificDbIsSatisfied () {
-		when:
+	def satisfiesDbCriteriaOrNot () {
+		given:
 			criteria = new DbCriteria (dbName)
-			
-		then:
-			criteria.isSatisfiedBy(documentOne)
-	}
-	
-	def returnsFalseIfCriteriaForASpecificDbisNotSatisfied () {
-		when:
-			criteria = new DbCriteria (dbName)
-		
-		then:
-			! criteria.isSatisfiedBy(documentThree)
+		expect:
+			outcome == criteria.isSatisfiedBy(document)
+		where:
+			document             |   outcome
+			documentOne          |   true
+			documentTwo          |   false
+			configurationEntry   |   false
 	}
 }
--- src/test/groovy/com/ee/tayra/io/criteria/TimestampCriteriaSpecs.groovy
+++ src/test/groovy/com/ee/tayra/io/criteria/TimestampCriteriaSpecs.groovy
@@ -7,60 +7,25 @@ import com.ee.tayra.io.criteria.TimestampCriteria;
 import spock.lang.Specification
 
 class TimestampCriteriaSpecs extends Specification {
+	static document = '{ts:{$ts:1357537752,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
+	static timestampJsonOne = '{ts:{$ts:1357537752,$inc:2}}'
+	static timestampJsonTwo = '{ts:{$ts:1300000000,$inc:1}}'
+	static timestampIsoOne = '2013-01-08T15:19:40Z'
+	static timestampIsoTwo = '2012-12-26T15:19:40Z'
+	
 
-	def returnsTrueIfDocumentIsEarlierThanTimestamp () {
-		
+	def satisfiesTimestampCriteriaOrNot () {
 		given: 'A timestamp and an oplog document'
-			def timeStamp = '{ts:{$ts:1357537752,$inc:2}}'
-			def document = '{ts:{$ts:1357537752,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-		
-		when: 'timestamp criteria is applied to the document'
-			TimestampCriteria criteria = new TimestampCriteria(timeStamp)
-								
-		then: 'it returns true if the document is older than the timestamp'
-			criteria.isSatisfiedBy(document)
-		
-	}
-
-	def returnsFalseIfDocumentIsLaterThanTimestamp () {
-		
-		given: 'A timestamp and an oplog document'
-			def timeStamp = '{ts:{$ts:1300000000,$inc:1}}'
-			def document = '{ts:{$ts:1357537752,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-		
-		when: 'timestamp criteria is applied to the document'
-			TimestampCriteria criteria = new TimestampCriteria(timeStamp)
-								
-		then: 'it returns true if the document is older than the timestamp'
-			! criteria.isSatisfiedBy(document)
-		
-	}
-
-	def returnsTrueIfDocumentIsEarlierThanISOTime() {
-		
-		given: 'A timestamp and an oplog document'
-			def timeStamp = '2012-12-26T15:19:40Z'
-			def document = '{ts:{$ts:1356515303,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-		
-		when: 'timestamp criteria is applied to the document'
-			TimestampCriteria criteria = new TimestampCriteria(timeStamp)
-								
-		then: 'it returns true if the document is older than the timestamp'
-			criteria.isSatisfiedBy(document)
-		
-	}
-
-	def returnsFalseIfDocumentIsLaterThanISOTime() {
-		
-		given: 'A timestamp and an oplog document'
-			def timeStamp = '2012-12-26T15:19:40Z'
-			def document = '{ts:{$ts:1357801207,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-		
-		when: 'timestamp criteria is applied to the document'
-			TimestampCriteria criteria = new TimestampCriteria(timeStamp)
-								
-		then: 'it returns true if the document is older than the timestamp'
-			!criteria.isSatisfiedBy(document)
+			TimestampCriteria criteria = new TimestampCriteria(timestamp)
+		expect:
+			outcome == criteria.isSatisfiedBy(document)
+			
+		where:
+			timestamp        |     outcome
+			timestampJsonOne     |     true
+			timestampJsonTwo     |     false
+			timestampIsoOne      |     true
+			timestampIsoTwo      |     false
 	}
 
 }
