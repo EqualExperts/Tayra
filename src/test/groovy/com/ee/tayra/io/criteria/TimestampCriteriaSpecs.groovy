fa0baaa29318f6945ae9ad3e22ff0d83539cecc0
Bhagyashree <bhagyashree.mandora@gmail.com>
2013-03-13 11:36:26 +0530

    added specs for invalid timestamp format

--- src/test/groovy/com/ee/tayra/io/criteria/TimestampCriteriaSpecs.groovy
+++ src/test/groovy/com/ee/tayra/io/criteria/TimestampCriteriaSpecs.groovy
@@ -110,11 +110,50 @@ class TimestampCriteriaSpecs extends Specification {
 		given: 'A timestamp and an oplog document'
 			def timeStamp = '2012-12-26T15:19:40Z'
 			def document = '{ts:{$ts:1357801207,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
-		
+
 		when: 'timestamp criteria is applied to the document with sExclude as true'
 			TimestampCriteria criteria = new TimestampCriteria(timeStamp, true)
-								
+
 		then: 'document is not excluded if it is later than the timestamp'
 			criteria.isSatisfiedBy(document)
+	}
+
+	def shoutsWhenInvalidJSONTimestampFormatIsGiven() {
+		given:'an invalid JSON timestamp'
+			def invalidTimeStamp = '{ts:{$s:1357801207,$inc:1}}'
+
+		when: 'timestamp criteria is applied to the document'
+			TimestampCriteria criteria = new TimestampCriteria(invalidTimeStamp, true)
+
+		then: 'it shouts'
+			thrown Exception
+	}
+
+	def shoutsWhenInvalidISOTimestampFormatIsGiven() {
+		given:'an invalid ISO timestamp'
+			def invalidTimeStamp = '2012-12-2615:19:40Z'
+
+		when: 'timestamp criteria is applied to the document'
+			TimestampCriteria criteria = new TimestampCriteria(invalidTimeStamp, true)
+
+		then: 'it shouts'
+			thrown Exception
+	}
+
+	def shoutsWhenDocumentHasInvalidTimestampFormat() {
+		given:'A timestamp'
+			def timeStamp = '{ts:{$ts:1357801207,$inc:1}}'
+			
+		and: 'an oplog document with an invalid timestamp'
+			def document = '{ts:{$s:1357801207,$inc:1} , "h" : -2719432537158937612 , "v" : 2 , "op" : "i" , "ns" : "test.things" , "o" : { "_id" : { "$oid" : "50ea61d85bdcefd43e2994ae"} , "roll" : 0.0}}'
+
+		and: 'a timestamp criteria'
+			TimestampCriteria criteria = new TimestampCriteria(timeStamp, true)
+
+		when: 'timestamp criteria is applied to the document'
+			criteria.isSatisfiedBy(document)
+
+		then: 'it shouts'
+			thrown Exception
 	}
 }
