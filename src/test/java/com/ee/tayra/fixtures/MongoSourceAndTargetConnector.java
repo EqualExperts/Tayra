e9384b4d5b0f981cd519e5379fc688dead29006f
samridhi.ee <sgupta@equalexperts.com>
2013-03-28 11:58:23 +0530

    refactored MongoSourceandTargrtConnector to make ore readable

--- src/test/java/com/ee/tayra/fixtures/MongoSourceAndTargetConnector.java
+++ src/test/java/com/ee/tayra/fixtures/MongoSourceAndTargetConnector.java
@@ -19,26 +19,42 @@ public class MongoSourceAndTargetConnector {
     authenticateMongoClients(namedParams);
   }
 
+  private void createMongoClientsFrom(final String cmdString,
+      final NamedParameters namedParams) throws UnknownHostException {
+    List<String> userKeys = extractUserKeysFrom(cmdString);
+    String srcNode = namedParams.get(userGivenKeyFor("SrcNode", userKeys));
+    String srcPort = namedParams.get(userGivenKeyFor("SrcPort", userKeys));
+    String tgtNode = namedParams.get(userGivenKeyFor("TgtNode", userKeys));
+    String tgtPort = namedParams.get(userGivenKeyFor("TgtPort", userKeys));
+    source = new MongoClient(srcNode, Integer.parseInt(srcPort));
+    destination = new MongoClient(tgtNode, Integer.parseInt(tgtPort));
+  }
+
+private List<String> extractUserKeysFrom(final String cmdString) {
+    List<String> userKeys = new ArrayList<String>();
+    for (String key : cmdString.split(" ")) {
+      if (key.contains("{")) {
+        userKeys.add(key);
+      }
+    }
+  return userKeys;
+}
+
+  private String userGivenKeyFor(final String key,
+                                 final List<String> userKeys) {
+    for (String userKey : userKeys) {
+      if (userKey.contains(key)) {
+        return userKey;
+      }
+    }
+    return key;
+  }
+
   private void authenticateMongoClients(final NamedParameters namedParams) {
     new MongoAuthenticator(source).authenticate(
         namedParams.get("{username}"), namedParams.get("{password}"));
     new MongoAuthenticator(destination).authenticate(
         namedParams.get("{username}"), namedParams.get("{password}"));
-  }
-
-  private void createMongoClientsFrom(final String cmdString,
-      final NamedParameters namedParams) throws UnknownHostException {
-    List<String> params = new ArrayList<String>();
-    for (String param : cmdString.split(" ")) {
-      if (param.contains("{")) {
-        params.add(param);
-      }
-    }
-    int index = 0;
-    source = new MongoClient(namedParams.get(params.get(index++)),
-        Integer.parseInt(namedParams.get(params.get(index++))));
-    destination = new MongoClient(namedParams.get(params.get(index++)),
-        Integer.parseInt(namedParams.get(params.get(index))));
   }
 
   public final MongoClient getSource() {
