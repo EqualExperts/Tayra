2074eef3be720f275c0473849161df1bccef7552
samridhi.ee <sgupta@equalexperts.com>
2013-04-02 18:10:18 +0530

    test added for -fAll with dry-run

--- exploratory-tests/test/groovy/com/ee/tayra/exploratory/ExploratoryTestSupport.groovy
+++ exploratory-tests/test/groovy/com/ee/tayra/exploratory/ExploratoryTestSupport.groovy
@@ -27,7 +27,7 @@ class ExploratoryTestSupport extends Specification {
   private static File timestampFile = new File('timestamp.out')
 
   def setupSpec() throws UnknownHostException, MongoException {
-	updateTimestampFileToNow()
+    updateTimestampFileToNow()
 
     String cmdString = '''source {unsecureSrcNode} on port {unsecureSrcPort} ,
                    target {unsecureTgtNode} on port {unsecureTgtPort}'''
@@ -35,27 +35,30 @@ class ExploratoryTestSupport extends Specification {
     connector = factory.createMongoSourceTargetConnector(cmdString)
     src = connector.source.mongo
     tgt = connector.target.mongo
+
     srcHOST = src.getAddress().host
     srcPORT = src.getAddress().port
     tgtHOST = tgt.getAddress().host
     tgtPORT = tgt.getAddress().port
-	addDataTo(src)
+
+    addDataTo(src)
   }
 
   def cleanupSpec() {
     timestampFile.delete()
-	deleteDataFrom(src)
+    deleteBackupFiles(0)
+    deleteDataFrom(src)
     connector.close()
   }
-//
-//  def setup() {
-//    new File('timestamp.out').text = timestamp
-//  }
 
-  private updateTimestampFileToNow() {
-	  String time = '{ "ts" : { "$ts" : ' + ((int)(System.currentTimeMillis()/1000)) + ', "$inc" : 0} }'
-			  timestampFile.text = time
-			  timestamp = timestampFile.text
+  def setup() {
+    new File('timestamp.out').text = timestamp
+  }
+
+  def updateTimestampFileToNow() {
+    String time = '{ "ts" : { "$ts" : ' + ((int)(System.currentTimeMillis()/1000)) + ', "$inc" : 0} }'
+    timestampFile.text = time
+    timestamp = timestampFile.text
   }
 
   private final void run(final String cmdString, final DB db) {
@@ -71,23 +74,49 @@ class ExploratoryTestSupport extends Specification {
     backupContext.setVariable('args', ['-s', srcHOST, "--port=$srcPORT", '-f', backupFile])
     new Backup(backupContext).run()
   }
-  
-  private void addDataTo(MongoClient mongoNode) {
-	  DB DLDb = mongoNode.getDB("DL")
-	  DB TayraDb = mongoNode.getDB("Tayra")
-	  DB EELabDb = mongoNode.getDB("EELab")
-	  run('db.profile.insert({name:"One"})', DLDb)
-	  run('db.thing.insert({name:"One"})', DLDb)
-	  run('db.profile.insert({name:"One"})', TayraDb)
-	  run('db.thing.insert({name:"One"})', TayraDb)
-	  run('db.profile.insert({name:"One"})', EELabDb)
-	  run('db.thing.insert({name:"One"})', EELabDb)
-	}
-  
-	def deleteDataFrom(MongoClient mongoNode) {
-	  mongoNode.getDB("DL").dropDatabase()
-	  mongoNode.getDB("Tayra").dropDatabase()
-	  mongoNode.getDB("EELab").dropDatabase()
-	}
-}
 
+  private addDataTo(MongoClient mongoNode) {
+    DB DLDb = mongoNode.getDB("DL")
+    DB TayraDb = mongoNode.getDB("Tayra")
+    DB EELabDb = mongoNode.getDB("EELab")
+    run('db.profile.insert({name:"One"})', DLDb)
+    run('db.thing.insert({name:"One"})', DLDb)
+    run('db.profile.insert({name:"One"})', TayraDb)
+    run('db.thing.insert({name:"One"})', TayraDb)
+    run('db.profile.insert({name:"One"})', EELabDb)
+    run('db.thing.insert({name:"One"})', EELabDb)
+  }
+
+  protected void deleteDataFrom(MongoClient mongoNode) {
+    mongoNode.getDB("DL").dropDatabase()
+    mongoNode.getDB("Tayra").dropDatabase()
+    mongoNode.getDB("EELab").dropDatabase()
+  }
+
+  protected void addExtraDataTo(MongoClient mongoNode) {
+    DB ExtraDb = mongoNode.getDB("Extra")
+    run('db.thing.insert({name:"One"})', ExtraDb)
+    run('db.thing.insert({name:"Two"})', ExtraDb)
+    run('db.thing.insert({name:"Three"})', ExtraDb)
+    run('db.thing.insert({name:"Four"})', ExtraDb)
+    run('db.thing.insert({name:"Five"})', ExtraDb)
+    run('db.thing.insert({name:"Six"})', ExtraDb)
+  }
+  
+  protected void deleteExtraDataFrom(MongoClient mongoNode) {
+    mongoNode.getDB("Extra").dropDatabase()
+  }
+
+  protected void deleteBackupFiles(int noOfFiles) {
+    File backup = new File(backupFile)
+    if(backup.exists()) {
+      backup.delete()
+    }
+    for(int i=1; i<=noOfFiles; i++) {
+      File file = new File(backupFile + "." + i)
+      if(file.exists()) {
+        file.delete()
+      }
+    }
+  }
+}
--- exploratory-tests/test/groovy/com/ee/tayra/exploratory/RestoreExploratoryTest.groovy
+++ exploratory-tests/test/groovy/com/ee/tayra/exploratory/RestoreExploratoryTest.groovy
@@ -1,19 +1,16 @@
 package com.ee.tayra.exploratory
 
+import com.ee.tayra.command.backup.Backup
 import com.ee.tayra.command.restore.Restore
+import com.mongodb.MongoClient;
 
 class RestoreExploratoryTest extends ExploratoryTestSupport {
 
   private def context
-  private static StringBuilder result
+  static StringBuilder result
 
   def setupSpec() {
-	takeBackup()
-
-    ExpandoMetaClass.enableGlobally()
-    PrintWriter.metaClass.println = { String data ->
-      result << data
-    }
+    takeBackup()
   }
 
   def setup() {
@@ -21,17 +18,17 @@ class RestoreExploratoryTest extends ExploratoryTestSupport {
     context = new Binding()
   }
 
-  private void cleanup() {
+  def cleanup() {
     deleteDataFrom(tgt)
   }
 
   def restoresOnNonAuthenticatedStandaloneWhenCredentialsAreGiven() {
     given: 'arguments for restore contains -d, --port, -f, --sExclude, --sNs options'
       context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '-u', USERNAME, '-p', PASSWORD])
-  
+
     when: 'restore runs with above args'
       new Restore(context).run()
-  
+
     then: 'the target Node should contain all documents'
       assertTargetNodeContainsAllDocuments()
   }
@@ -42,7 +39,7 @@ class RestoreExploratoryTest extends ExploratoryTestSupport {
 
     when: 'restore runs with above args'
       new Restore(context).run()
-    
+
     then: 'the target Node should not contain any document'
       !assertTargetNodeContainsAllDocuments()
   }
@@ -52,71 +49,97 @@ class RestoreExploratoryTest extends ExploratoryTestSupport {
       context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sExclude', '--sNs=Tayra,EELab.thing', '--dry-run'])
 
     when: 'restore runs with above args'
-      new Restore(context).run()
+      result << restoreAndCaptureConsoleOutput(context)
 
-    then: 'the target Node should not contain any document'
-//      !result.toString().contains('"ns" : "Tayra.')
-//      !result.toString().contains('"ns" : "EELab.thing"')
+    then: 'the target Node should not contain document having ns as Tayra and EElab.thing'
+      !result.toString().contains('"ns" : "Tayra.')
+      !result.toString().contains('"ns" : "EELab.thing"')
+      result.toString().contains('"ns" : "EELab.profile"')
+      result.toString().contains('"ns" : "DL.')
   }
 
   def excludesDocumentsbelongingToMultipleNsWithSExcludeWhileRestore() {
-	  given: 'arguments for restore contains -d, --port, -f, --sExclude, --sNs options'
-		context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sExclude', '--sNs=DL,EELab.thing'])
-  
-	  when: 'backup runs with above args'
-		new Restore(context).run()
-  
-	  then: 'the target Node should not contain documents having ns as DL and EELab.thing'
-		!tgt.getDatabaseNames().contains("DL")
-		tgt.getDB("EELab").getCollection("thing").count() == 0
-	}
-  
-	def noCriteriaIsAppliedWhenNoValueIsGivenforSns() {
-	  given:'arguments contains -s, --port, -f, --sNs options'
-		context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sNs='])
-  
-	  when: 'Restore runs'
-		new Restore(context).run()
-  
-	  then: 'the target node should contain all documents'
-	    assertTargetNodeContainsAllDocuments()
-	}
-  
-	def allDocumentsAreRestoredWhenNoValueIsGivenforSUntil() {
-	  given: 'arguments for restore contains -f, -d, --port, -f, --sUntil options'
-		context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sUntil='])
-  
-	  when: 'restore runs'
-		new Restore(context).run()
-  
-	  then: 'the target Node should contain all documents'
-		assertTargetNodeContainsAllDocuments()
-	}
+    given: 'arguments for restore contains -d, --port, -f, --sExclude, --sNs options'
+      context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sExclude', '--sNs=DL,EELab.thing'])
+
+    when: 'backup runs with above args'
+      new Restore(context).run()
+
+    then: 'the target Node should not contain documents having ns as DL and EELab.thing'
+      !tgt.getDatabaseNames().contains("DL")
+      tgt.getDB("EELab").getCollection("thing").count() == 0
+  }
+
+  def noCriteriaIsAppliedWhenNoValueIsGivenforSns() {
+    given:'arguments contains -s, --port, -f, --sNs options'
+      context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sNs='])
+
+    when: 'Restore runs'
+      new Restore(context).run()
+
+    then: 'the target node should contain all documents'
+      assertTargetNodeContainsAllDocuments()
+  }
+
+  def allDocumentsAreRestoredWhenNoValueIsGivenforSUntil() {
+    given: 'arguments for restore contains -f, -d, --port, -f, --sUntil options'
+      context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--sUntil='])
+
+    when: 'restore runs'
+      new Restore(context).run()
+
+    then: 'the target Node should contain all documents'
+      assertTargetNodeContainsAllDocuments()
+  }
+
+  def allBackupFilesAreReadWhenFAlleIsGivenWithDryRunOption() {
+    given:'Backup is taken'
+      addExtraDataTo(src)
+      Binding backupContext = new Binding()
+      backupContext.setVariable('args', ['-s', srcHOST, "--port=$srcPORT", '-f', backupFile, '--fSize=1KB', '--fMax=5'])
+      new Backup(backupContext).run()
+
+    and:'arguments for restore contains -d, --port, -f, --dry-run options'
+      context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--fAll', '--dry-run'])
+
+    when: 'restore runs with above args'
+      result << restoreAndCaptureConsoleOutput(context)
+
+    then: 'the result should contain all documents'
+      result.toString().contains('"ns" : "Tayra.thing"')
+      result.toString().contains('"ns" : "Tayra.profile"')
+      result.toString().contains('"ns" : "EELab.thing')
+      result.toString().contains('"ns" : "EELab.profile')
+      result.toString().contains('"ns" : "DL.thing')
+      result.toString().contains('"ns" : "DL.profile')
+
+    cleanup: 'delete extra data and backup files'
+      deleteExtraDataFrom(src)
+      deleteBackupFiles(5)
+  }
 
   private assertTargetNodeContainsAllDocuments() {
-	  tgt.getDB("DL").getCollection("profile").count == 1
-	  (tgt.getDB("DL").getCollection("thing").count()) == 1
-	  (tgt.getDB("Tayra").getCollection("profile").count()) == 1
-	  (tgt.getDB("Tayra").getCollection("thing").count()) == 1
-	  (tgt.getDB("EELab").getCollection("profile").count()) == 1
-	  (tgt.getDB("EELab").getCollection("thing").count()) == 1
+    tgt.getDB("DL").getCollection("profile").count == 1
+    tgt.getDB("DL").getCollection("thing").count() == 1
+    tgt.getDB("Tayra").getCollection("profile").count() == 1
+    tgt.getDB("Tayra").getCollection("thing").count() == 1
+    tgt.getDB("EELab").getCollection("profile").count() == 1
+    tgt.getDB("EELab").getCollection("thing").count() == 1
+  }
+
+  private restoreAndCaptureConsoleOutput(def context) {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    PrintStream ps = new PrintStream(baos);
+    // IMPORTANT: Save the old System.out!
+    PrintStream old = System.out;
+    // use your special stream
+    System.setOut(ps);
+
+    new Restore(context).run()
+
+    // Put things back
+    System.out.flush();
+    System.setOut(old);
+    baos.toString()
   }
 }
-
-//  def allBackupFilesAreReadWhenFAlleIsGivenWithDryRunOption() {
-//	  given:'Backup is taken'
-////	    addDataTo(src)
-//	    Binding backupContext = new Binding()
-//        backupContext.setVariable('args', ['-s', srcHOST, "--port=$srcPORT", '-f', backupFile, '--fSize=1KB', '--fMax=3'])
-//        new Backup(backupContext).run()
-//  
-//	  and:'arguments for restore contains -d, --port, -f, --dry-run options'
-//		context.setVariable('args', ['-d', tgtHOST, "--port=$tgtPORT", '-f', backupFile, '--fAll', '--dry-run'])
-//  
-//	  when: 'restore runs with above args'
-//		new Restore(context).run()
-//  
-//	  then: 'the target Node should not contain any document'
-////		!result.toString().contains('"ns" : "Tayra.')
-////		!result.toString().contains('"ns" : "EELab.thing"')
-//	}
