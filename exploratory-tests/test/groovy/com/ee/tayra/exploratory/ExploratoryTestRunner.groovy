28bf3047f7d1fbf19ab00192930b906deedb6def
Bhagyashree <bhagyashree.mandora@gmail.com>
2013-04-26 17:26:09 +0530

    gradlized exploratory tests

---
+++ exploratory-tests/test/groovy/com/ee/tayra/exploratory/ExploratoryTestRunner.groovy
@@ -0,0 +1,11 @@
+package com.ee.tayra.exploratory
+
+import org.junit.runner.JUnitCore
+
+class ExploratoryTestRunner {
+
+	public static void main(String[] args) throws Exception {
+		JUnitCore.main("com.ee.tayra.exploratory.BackupExploratoryTest",
+"com.ee.tayra.exploratory.RestoreExploratoryTest");
+ }
+}
