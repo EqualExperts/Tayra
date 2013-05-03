c333808ee017144e9d254531150acf14ef47005c
samridhi.ee <sgupta@equalexperts.com>
2013-05-03 11:21:45 +0530

    Memory mapped test added to runner

--- exploratory-tests/test/groovy/com/ee/tayra/exploratory/ExploratoryTestRunner.groovy
+++ exploratory-tests/test/groovy/com/ee/tayra/exploratory/ExploratoryTestRunner.groovy
@@ -6,6 +6,7 @@ class ExploratoryTestRunner {
 
 	public static void main(String[] args) throws Exception {
 		JUnitCore.main("com.ee.tayra.exploratory.BackupExploratoryTest",
+"com.ee.tayra.exploratory.RestoreExploratoryTest",
 "com.ee.tayra.exploratory.RestoreExploratoryTest");
  }
 }
